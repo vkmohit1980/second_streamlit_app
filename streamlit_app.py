0d94b4a70e8dd54a76c1781f65caf0e74794b593
vkmohit1980 <61517912+vkmohit1980@users.noreply.github.com>
2023-09-30 15:03:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
+import pandas
 
-streamlit.title('My parents new healthy Diner')
-streamlit.header('Menu')
-streamlit.header('Omlette')
-streamlit.header('Mushrooms and Juice')
-streamlit.header('Veggie burger')
-
+my_fruit_list = my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
